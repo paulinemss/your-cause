e1d38fc73835f2f63ff782be9638e853e9092d6c
paulinemss <masse.pauline@gmail.com>
2020-11-23 21:50:29 +0100

    Added news

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router  = express.Router();
 const Event = require('../models/Event.model');
+const Parser = require('rss-parser');
+const parser = new Parser();
 
 const shouldNotBeLoggedIn = require('../middlewares/shouldNotBeLoggedIn');
 const isLoggedIn = require('../middlewares/isLoggedIn');
+
+const topic = {
+  environment: 'CAAqBwgKMKeh0wEw-sE1',
+  women: 'CAAqIQgKIhtDQkFTRGdvSUwyMHZNREpmYURBU0FtVnVLQUFQAQ'
+}
 
 const { getOnlyInterestingEvents,
 removeOldEvents,
@@ -22,9 +29,12 @@ router.get('/', (req, res, next) => {
 router.get('/dash', isLoggedIn, (req, res, next) => {
   const { user } = req.session; 
 
-  Event
-    .find()
-    .then(foundEvents => {
+  const events = Event.find();
+  const news = parser.parseURL(`https://news.google.com/rss/topics/${topic[user.interest]}?hl=en-US&gl=US&ceid=US:en`); 
+
+  Promise.all([events, news])
+    .then(values => {
+      const [foundEvents, foundNews] = values; 
 
       /* sorting the array of found events */ 
       const withoutOldEvents = removeOldEvents(foundEvents);
@@ -33,13 +43,16 @@ router.get('/dash', isLoggedIn, (req, res, next) => {
       /* finding all events for the main feed */ 
       const mainEvents = getOnlyInterestingEvents(sortedEvents, user.interest);
 
-      /* slicing the events array to only show the first one */
+      /* slicing the events array to only show the first 5 */
       const events = mainEvents.slice(0, 5);
 
-      res.render('dash', { user, events });
+      /* slicing the news array to only show the first 5 */
+      const news = foundNews.items.slice(0, 5);
+
+      res.render('dash', { user, events, news });
     })
     .catch(err => {
-      console.log('error finding events on dash page', err); 
+      console.log('error finding databases on dash page', err); 
     })
 });
 

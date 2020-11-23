e8222d20a849dc7a04865a00c9769930e930e0a8
paulinemss <masse.pauline@gmail.com>
2020-11-23 10:38:59 +0100

    remove comment

--- models/User.model.js
+++ models/User.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
     type: String,

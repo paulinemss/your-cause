ea39c8fa4f84d011d0c7e054cdee661e3e3c77aa
Monique de Haard <moniquedehaard@msn.com>
2020-11-24 15:19:21 +0100

    Books feed and book-page are done

--- routes/books.js
+++ routes/books.js
@@ -6,7 +6,8 @@ const User = require('../models/User.model')
 
 // TODO
 // 1. See if user is loggedin ✅ 
-// 2. check users preferences
+// 2. check users preferences ✅ 
+// 3. links should be updated ✅ 
 // 3. Need the first 10 books of today
 
 
@@ -60,39 +61,33 @@ router.get('/', isLoggedIn, (req,res,next) => {
   .catch(err => console.log(err))
 })
 
-router.get('/:id/like-book', isLoggedIn, (req,res,next) => {
-  const { user } = req.session;
-  const { id } = req.params;
-
-  Book
-  .findById(id)
-  .then(foundBook => {
-    console.log('Book found: ', foundBook);
-    res.render('books/book-page', { book: foundBook,  })
-  })
-  .catch(err => {
-    console.log('error finding the event to edit', err);
-  })
-})
-
-
 
 /// BOOK PAGE /// 
 router.get('/:id', isLoggedIn, (req,res,next) => {
   const { user } = req.session;
   const { id } = req.params;
 
+  const { savedBooks, readBooks } = user;
+
   Book
   .findById(id)
   .then(foundBook => {
     console.log('Book found: ', foundBook);
-    res.render('books/book-page', { book: foundBook,  })
+
+    const bookIsSavedBook = savedBooks.filter(el => el.equals(foundBook._id))
+    const bookIsReadBook = readBooks.filter(el => el.equals(foundBook._id))
+    const isInSavedBooks =  bookIsSavedBook.length != 0 ? true : false;
+    const isInReadBooks = bookIsReadBook.length != 0 ? true : false;
+
+    res.render('books/book-page', { 
+      book: foundBook, 
+      isInSavedBooks: isInSavedBooks, 
+      isInReadBooks: isInReadBooks})
   })
   .catch(err => {
     console.log('error finding the event to edit', err);
   })
 })
-
 
 // When book is added to likes -> render page again with the new likes 
 router.get('/like-book/:id', isLoggedIn, (req, res) => {
@@ -138,7 +133,6 @@ router.get('/unlike-book/:id', isLoggedIn, (req, res) => {
     })
 });
 
-
 // When book is added to reading list render page again with the reading list
 router.get('/read-book/:id', isLoggedIn, (req, res) => {
   const { user } = req.session;
@@ -183,30 +177,93 @@ router.get('/unread-book/:id', isLoggedIn, (req, res) => {
     })
 });
 
-// When book is added to reading list render page again with the reading list
+// When you add a book to the savedBooks in book-page
 router.get('/book-page/like-book/:id', isLoggedIn, (req, res) => {
   const { user } = req.session;
   const { id } = req.params;
 
-  console.log(user)
-  console.log(id)
-  res.redirect('/books');
+  User
+    .findByIdAndUpdate(user._id,
+      {
+        $addToSet: { savedBooks: id }
+     },
+     { new: true }
+    )
+    .then(updatedUser => {
+      console.log('updatedUser: ', updatedUser);
+      req.session.user = updatedUser;
+      res.redirect(`/books`);
+    })
+    .catch(err => {
+      console.log("There has been an error while liking a book to read book ", err);
+    })
+});
 
-  // User
-  //   .findByIdAndUpdate(user._id, 
-  //     {
-  //        $addToSet: { readBooks: id }
-  //     },
-  //     { new: true }
-  //   )
-  //   .then(updatedUser => {
-  //     console.log('updatedUser: ', updatedUser);
-  //     req.session.user = updatedUser;
-  //     res.redirect(`/books`);
-  //   })
-  //   .catch(err => {
-  //     console.log("There has been an error while adding book to read book ", err);
-  //   })
-})
+// When you delete a book from the savedBooks in book-page
+router.get('/book-page/unlike-book/:id', isLoggedIn, (req, res) => {
+  const { user } = req.session;
+  const { id } = req.params;
+
+  User
+    .findByIdAndUpdate(user._id,
+      {
+        $pull: { savedBooks: id }
+     },
+     { new: true }
+    )
+    .then(updatedUser => {
+      console.log('updatedUser: ', updatedUser);
+      req.session.user = updatedUser;
+      res.redirect(`/books`);
+    })
+    .catch(err => {
+      console.log("There has been an error while liking a book to read book ", err);
+    })
+});
+
+// When you add a book to the readBooks in book-page
+router.get('/book-page/read-book/:id', isLoggedIn, (req, res) => {
+  const { user } = req.session;
+  const { id } = req.params;
+
+  User
+    .findByIdAndUpdate(user._id,
+      {
+        $addToSet: { readBooks: id }
+     },
+     { new: true }
+    )
+    .then(updatedUser => {
+      console.log('updatedUser: ', updatedUser);
+      req.session.user = updatedUser;
+      res.redirect(`/books`);
+    })
+    .catch(err => {
+      console.log("There has been an error while adding a book to the readArray", err);
+    })
+});
+
+// When you delete a book from the readBooks in book-page
+router.get('/book-page/unread-book/:id', isLoggedIn, (req, res) => {
+  const { user } = req.session;
+  const { id } = req.params;
+
+  User
+    .findByIdAndUpdate(user._id,
+      {
+        $pull: { readBooks: id }
+     },
+     { new: true }
+    )
+    .then(updatedUser => {
+      console.log('updatedUser: ', updatedUser);
+      req.session.user = updatedUser;
+      res.redirect(`/books`);
+    })
+    .catch(err => {
+      console.log("There has been an error while deleting a book from the readArray", err);
+    })
+});
+
 
 module.exports = router
